b70bcfc08144adf9b6bd65a4e2237572947fa744
Munetaka <sakuraapp1988@gmail.com>
2020-06-21 21:56:32 +0900

    Add sample page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,6 +13,10 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Link href="/sample/index">
+          <button>サンプルページへ</button>
+        </Link>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
