8c4968a1affc8c520da2d6f86d8c2b130715dadb
Munetaka <sakuraapp1988@gmail.com>
2020-07-06 21:34:53 +0900

    Add css-lib bulma

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+import 'bulma/css/bulma.min.css'
+
+// eslint-disable-next-line react/prop-types
+export default function MyApp ({ Component, pageProps }) {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+}
