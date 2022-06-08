72e79c67b83fdd07045549665a7c65a3d8719e4f
Munetaka <sakuraapp1988@gmail.com>
2022-06-08 23:03:40 +0900

    Set configuration and format code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,15 +13,11 @@ const Home: NextPage = () => (
 
     <main className={styles.main}>
       <h1 className={styles.title}>
-        Welcome to
-        {' '}
-        <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className={styles.description}>
-        Get started by editing
-        {' '}
-        <code className={styles.code}>pages/index.tsx</code>
+        Get started by editing <code className={styles.code}>pages/index.tsx</code>
       </p>
 
       <div className={styles.grid}>
@@ -35,10 +31,7 @@ const Home: NextPage = () => (
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
+        <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
@@ -48,9 +41,7 @@ const Home: NextPage = () => (
           className={styles.card}
         >
           <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
       </div>
     </main>
@@ -61,8 +52,7 @@ const Home: NextPage = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by
-        {' '}
+        Powered by {' '}
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
