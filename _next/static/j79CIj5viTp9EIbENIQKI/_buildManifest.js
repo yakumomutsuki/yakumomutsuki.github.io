56c73d7dda88e41b8be7a7f470e16344964d9733
yakumomutsuki <yakumomutsuki@users.noreply.github.com>
2022-06-28 13:39:30 +0000

    deploy: 24e1343f332e87fcb3c6eb8df68adc825f093466

---
+++ _next/static/j79CIj5viTp9EIbENIQKI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0b300611126d5f38.css","static/chunks/pages/index-e6511fbe14588334.js"],"/_error":["static/chunks/pages/_error-815e492bede44f3e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
